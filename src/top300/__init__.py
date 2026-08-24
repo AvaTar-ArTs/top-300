4fe4f8fe8de744612afcf9e8e8ef0adfc33b0083
AvaTar-ArTs <me@avatararts.org>
2026-08-24 15:00:07 -0400

    feat: expose TOP-300 public API

---
+++ src/top300/__init__.py
@@ -0,0 +1,12 @@
+from .core import finite_differences, opportunity_ratio, percent_growth, score_snapshot
+from .models import FeatureSnapshot, ForecastResult, TrendState
+
+__all__ = [
+    "FeatureSnapshot",
+    "ForecastResult",
+    "TrendState",
+    "finite_differences",
+    "opportunity_ratio",
+    "percent_growth",
+    "score_snapshot",
+]
